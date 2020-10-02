8252963b3e70df07b4f22cc153aa7582b5a30649
Reid Chan <reidchan@apache.org>
2020-10-02 19:26:47 +0800

    Add checkAndXXX metrics

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/RSRpcServices.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/RSRpcServices.java
@@ -2599,7 +2599,8 @@ public class RSRpcServices implements HBaseRPCErrorHandler,
         switch (type) {
         case DELETE:
           if (request.hasCondition()) {
-            regionServer.metricsRegionServer.updateCheckAndDelete(after - before);
+            regionServer.metricsRegionServer.updateCheckAndDelete(
+                region == null ? null : region.getRegionInfo().getTable(), after - before);
           } else {
             regionServer.metricsRegionServer.updateDelete(
                 region == null ? null : region.getRegionInfo().getTable(), after - before);
@@ -2607,10 +2608,11 @@ public class RSRpcServices implements HBaseRPCErrorHandler,
           break;
         case PUT:
           if (request.hasCondition()) {
-            regionServer.metricsRegionServer.updateCheckAndPut(after - before);
+            regionServer.metricsRegionServer.updateCheckAndPut(
+                region == null ? null : region.getRegionInfo().getTable(), after - before);
           } else {
             regionServer.metricsRegionServer.updatePut(
-                region == null ? null : region.getRegionInfo().getTable(),after - before);
+                region == null ? null : region.getRegionInfo().getTable(), after - before);
           }
           break;
         default:
