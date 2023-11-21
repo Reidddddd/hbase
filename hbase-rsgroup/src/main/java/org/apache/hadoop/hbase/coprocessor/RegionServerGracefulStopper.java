5c8f58f2b638cd2d7c436a07cbd82ac2140f1f1d
Sean Xiao Yutong <sean.xiaoyt@shopee.com>
2023-11-21 16:45:53 +0800

    SPDI-105456 Implement a coprocessor to move online regions to other RS gracefully before RS stop

---
+++ hbase-rsgroup/src/main/java/org/apache/hadoop/hbase/coprocessor/RegionServerGracefulStopper.java
@@ -0,0 +1,89 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.hadoop.hbase.coprocessor;
+
+import java.io.IOException;
+import java.util.Optional;
+import org.apache.hadoop.hbase.rsgroup.RSGroupAdminClient;
+import org.apache.hadoop.hbase.rsgroup.RSGroupInfo;
+import org.apache.hbase.thirdparty.com.google.common.collect.Sets;
+import org.apache.yetus.audience.InterfaceAudience;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * A coprocessor to move the RS to a backup rsgroup and wait all the regions on it moved away.
+ */
+@InterfaceAudience.Private
+public class RegionServerGracefulStopper implements RegionServerObserver, RegionServerCoprocessor {
+  private static final Logger LOG = LoggerFactory.getLogger(RegionServerGracefulStopper.class);
+  private static final String HBASE_BACKUP_GROUP = "hbase.rs.graceful.stopper.backup.group";
+  private static final String DEFAULT_BACKUP_GROUP_NAME = "backup";
+
+  @Override
+  public Optional<RegionServerObserver> getRegionServerObserver() {
+    return Optional.of(this);
+  }
+
+  @Override
+  public void preStopRegionServer(ObserverContext<RegionServerCoprocessorEnvironment> context)
+    throws IOException {
+    RegionServerCoprocessorEnvironment rsEnv = context.getEnvironment();
+    RSGroupAdminClient rsGroupAdminClient = new RSGroupAdminClient(rsEnv.getConnection());
+    String backupGroup = rsEnv.getConfiguration().get(HBASE_BACKUP_GROUP,
+      DEFAULT_BACKUP_GROUP_NAME);
+    if (backupGroup != null) {
+      LOG.info("Start moving regions gracefully.");
+      RSGroupInfo origin =
+        rsGroupAdminClient.getRSGroupOfServer(rsEnv.getServerName().getAddress());
+      try {
+        rsGroupAdminClient.moveServers(Sets.newHashSet(rsEnv.getServerName().getAddress()),
+          backupGroup);
+      } catch (Exception e) {
+        // We do not have time to handle any exception. Just return.
+        return;
+      }
+      int checkTimes = 0;
+
+      // We check online regions every 10 seconds and wait maximum 1200 seconds to wait regions
+      // moving away.
+      // Do not worry about long time waiting, the pod will be recycled right away when we finish
+      // stopping.
+      while (!rsEnv.getOnlineRegions().getRegions().isEmpty() && checkTimes < 120) {
+        // Regions are still moving. Wait.
+        try {
+          checkTimes++;
+          Thread.sleep(10000);
+        } catch (Exception e) {
+          // We got exception cannot wait the region move. Stop.
+          break;
+        }
+      }
+      try {
+        // Balance the original group to eliminate the region skew.
+        if (origin != null) {
+          rsGroupAdminClient.balanceRSGroup(origin.getName());
+        }
+      } catch (Exception e) {
+        // We do not have time to handle this. Skip.
+        LOG.info("Failed balancing group: " + origin, e);
+      }
+    }
+  }
+}
---
+++ hbase-rsgroup/src/test/java/org/apache/hadoop/hbase/rsgroup/TestRegionServerGracefulStopper.java
@@ -0,0 +1,77 @@
+/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.hadoop.hbase.rsgroup;
+
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.regionserver.HRegionServer;
+import org.apache.hadoop.hbase.testclassification.MediumTests;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hbase.thirdparty.com.google.common.collect.Sets;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+@Category({ MediumTests.class })
+public class TestRegionServerGracefulStopper extends TestRSGroupsBase {
+
+  @BeforeClass
+  public static void setupBeforeClass() throws Exception {
+    TEST_UTIL.getConfiguration().set("hbase.coprocessor.regionserver.classes",
+      "org.apache.hadoop.hbase.coprocessor.RegionServerGracefulStopper");
+    setUpTestBeforeClass();
+  }
+
+  @AfterClass
+  public static void tearDownAfterTest() throws Exception {
+    tearDownAfterClass();
+  }
+
+  @Test
+  public void testStopperHook() throws Exception {
+    TableName tableName = TableName.valueOf("testStopperHook");
+    byte[] family = Bytes.toBytes("cf");
+    TEST_UTIL.createMultiRegionTable(tableName, family, 100);
+    rsGroupAdmin.addRSGroup("backup");
+    HRegionServer regionServer = TEST_UTIL.getMiniHBaseCluster().getRegionServer(0);
+//    regionServer.getRegionServerCoprocessorHost().load(RegionServerGracefulStopper.class,
+//      0, regionServer.getConfiguration());
+
+    regionServer.stop("stop for testing");
+    TEST_UTIL.waitUntilAllRegionsAssigned(tableName);
+
+    Thread.sleep(1000);
+    RSGroupInfo rsGroupInfo = rsGroupAdmin.getRSGroupInfo("backup");
+
+    Assert.assertNotNull(rsGroupInfo);
+    Assert.assertTrue(rsGroupInfo.getServers().contains(regionServer.getServerName().getAddress()));
+    Assert.assertTrue(regionServer.getRegions().isEmpty());
+
+    TEST_UTIL.deleteTable(tableName);
+    TEST_UTIL.getMiniHBaseCluster().startRegionServer(regionServer.getServerName().getHostname(),
+      regionServer.getServerName().getPort());
+    rsGroupInfo = rsGroupAdmin.getRSGroupInfo("backup");
+    Assert.assertTrue(rsGroupInfo.containsServer(regionServer.getServerName().getAddress()));
+    rsGroupAdmin.moveServers(Sets.newHashSet(regionServer.getServerName().getAddress()), "default");
+    TEST_UTIL.waitUntilNoRegionsInTransition();
+    rsGroupAdmin.removeRSGroup("backup");
+  }
+}
