d60717faa47e5bb863f7719196506dcbcaf7128c
Sean Xiao Yutong <sean.xiaoyt@shopee.com>
2023-11-24 06:27:00 +0000

    SPDI-112990 Schema Service: Add a limit of task in SchemaProcessor to prevent...

--- hbase-server/src/main/java/org/apache/hadoop/hbase/schema/SchemaProcessor.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/schema/SchemaProcessor.java
@@ -19,6 +19,8 @@
 package org.apache.hadoop.hbase.schema;
 
 import static org.apache.hadoop.hbase.schema.SchemaService.MAX_COLUMNS_PER_TABLE_DEFAULT;
+import static org.apache.hadoop.hbase.schema.SchemaService.MAX_TASK_NUM;
+import static org.apache.hadoop.hbase.schema.SchemaService.MAX_TASK_NUM_DEFAULT;
 import static org.apache.hadoop.hbase.schema.SchemaService.NUM_THREADS_DEFAULT;
 import static org.apache.hadoop.hbase.schema.SchemaService.NUM_THREADS_KEY;
 import static org.apache.hadoop.hbase.schema.SchemaService.SCHEMA_TABLE_CF;
@@ -76,6 +78,7 @@ public class SchemaProcessor {
   private ClusterConnection connection;
   private volatile boolean inited = false;
   private int maxColumn;
+  private int maxTasks;
 
   private static class Processor {
     private static final SchemaProcessor SINGLETON = new SchemaProcessor();
@@ -93,6 +96,7 @@ public class SchemaProcessor {
     String environment = env ? "Master-" : "RegionServer-";
     int numHandlers = conf.getInt(NUM_THREADS_KEY, NUM_THREADS_DEFAULT);
     maxColumn = conf.getInt(SOFT_MAX_COLUMNS_PER_TABLE, MAX_COLUMNS_PER_TABLE_DEFAULT);
+    maxTasks = conf.getInt(MAX_TASK_NUM, MAX_TASK_NUM_DEFAULT);
     updateExecutor = new FastPathExecutor(numHandlers, environment + "SchemaUpdateHandler");
     updateExecutor.start();
     taskAcceptor = new FastPathExecutor(numHandlers, environment + "SchemaProcessHandler");
@@ -305,6 +309,12 @@ public class SchemaProcessor {
 
   public boolean isWideTable(TableName tableName) {
     return wideTableSet.contains(tableName);
+  }
+
+  // A check to confirm whether we keep accept tasks according to the ongoing tasks.
+  // For preventing OOM.
+  public boolean isAvailable() {
+    return taskAcceptor.numTasksInQueue() + updateExecutor.numTasksInQueue() < maxTasks;
   }
 
 }
