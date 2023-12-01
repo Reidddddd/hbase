7c41257967b4b4528413c3ec54ed8f85dd588fc6
Sean Xiao Yutong <sean.xiaoyt@shopee.com>
2023-12-01 07:51:49 +0000

    SPDI-113363 Reduce bytes copy when construct ActionProcessor object

--- hbase-common/src/main/java/org/apache/hadoop/hbase/schema/Famy.java
+++ hbase-common/src/main/java/org/apache/hadoop/hbase/schema/Famy.java
@@ -18,60 +18,23 @@
 
 package org.apache.hadoop.hbase.schema;
 
-import java.util.Comparator;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.yetus.audience.InterfaceAudience;
 import org.apache.yetus.audience.InterfaceStability;
 
 @InterfaceStability.Evolving
 @InterfaceAudience.Public
-public class Famy implements Comparable<Famy>, Comparator<Famy> {
-
-  private final byte[] family;
+public class Famy extends AbstractSchemaComponent {
 
   public Famy(String famy) {
     this(Bytes.toBytes(famy));
   }
 
   public Famy(byte[] famy) {
-    family = famy;
+    this(famy, 0, famy.length);
   }
 
-  public byte[] getFamily() {
-    return family;
-  }
-
-  @Override
-  public int hashCode() {
-    return Bytes.hashCode(family);
-  }
-
-  @Override
-  public boolean equals(Object other) {
-    if (this == other) {
-      return true;
-    }
-    if (!(other instanceof Famy)) {
-      return false;
-    }
-    return Bytes.equals(family, ((Famy) other).family);
-  }
-
-  @Override
-  public int compareTo(Famy other) {
-    if (this == other) {
-      return 0;
-    }
-    return Bytes.compareTo(family, other.family);
-  }
-
-  @Override
-  public int compare(Famy o1, Famy o2) {
-    return o1.compareTo(o2);
-  }
-
-  @Override
-  public String toString() {
-    return Bytes.toString(this.family);
+  public Famy(byte[] famy, int offset, int length) {
+    super(famy, offset, length);
   }
 }
