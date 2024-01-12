aa35a4014022b7f4739f83d9c45ff79b31190162
Tianhang Tang <tianhang.tang@shopee.com>
2024-01-12 09:01:54 +0000

    SPDI-117548 Expose the swallowed timeoutException information when writeWALMetadata

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/wal/AsyncProtobufLogWriter.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/wal/AsyncProtobufLogWriter.java
@@ -215,9 +215,12 @@ public class AsyncProtobufLogWriter extends AbstractProtobufLogWriter
       InterruptedIOException ioe = new InterruptedIOException();
       ioe.initCause(e);
       throw ioe;
-    } catch (ExecutionException | TimeoutException e) {
+    } catch (ExecutionException e) {
       Throwables.propagateIfPossible(e.getCause(), IOException.class);
       throw new RuntimeException(e.getCause());
+    } catch (TimeoutException e) {
+      throw new RuntimeException("Write WAL meta data met a timeoutException after "
+        + waitTimeout + " mill seconds.", e);
     }
   }
 
