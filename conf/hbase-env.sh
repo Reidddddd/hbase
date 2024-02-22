fbf2621ec6423f730f5ea5964f51b1200afe2e62
Tianhang Tang <tianhang.tang@shopee.com>
2024-02-22 10:43:04 +0000

    SPDI-120423 Support replication consumer in daemon shell

--- conf/hbase-env.sh
+++ conf/hbase-env.sh
@@ -45,6 +45,8 @@ export HBASE_OPTS="-XX:+UseConcMarkSweepGC"
 # Configure PermSize. Only needed in JDK7. You can safely remove it for JDK8+
 export HBASE_MASTER_OPTS="$HBASE_MASTER_OPTS -XX:PermSize=128m -XX:MaxPermSize=128m -XX:ReservedCodeCacheSize=256m"
 export HBASE_REGIONSERVER_OPTS="$HBASE_REGIONSERVER_OPTS -XX:PermSize=128m -XX:MaxPermSize=128m -XX:ReservedCodeCacheSize=256m"
+# Configure Independent Replication Consumer.
+# export $HBASE_REPLICATION_CONSUMER_OPTS="$HBASE_REPLICATION_CONSUMER_OPTS -XX:PermSize=128m -XX:MaxPermSize=128m -XX:ReservedCodeCacheSize=256m"
 
 # Uncomment one of the below three options to enable java garbage collection logging for the server-side processes.
 
